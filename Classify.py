a565190fcbb64344fc489696ca106d37f1eb74ca
Turki-AlTamimi <turkiis506@gmail.com>
2025-12-04 03:58:08 +0300

    Add files via upload
    The original files provided by the doctor.

---
+++ Classify.py
@@ -0,0 +1,179 @@
+# --------------------------------------------------------------------------------------------------------------
+#
+# Filename: Classify.py
+# Author: Shahid Alam (shalam3@gmail.com)
+# Dated: September, 3, 2025
+#
+# --- TASK 1 ---
+# What is a True Positive Rate (TPR)?
+# What is a False Positive Rate (FPR)?
+# What TPR and FPR is produced by the MLPClassifier?
+# Is it possible to improve these values and how, give two approaches?
+#
+# --- TASK 2 ---
+# Enhance the IDS by improving the results, i.e., TPR and FPR.
+# Implement five more classifiers and print the results for each classifier.
+# Change the test size to 50%, repeat the classification and print the results for each classifier.
+#
+# --------------------------------------------------------------------------------------------------------------
+
+from __future__ import print_function
+import sys, math, time
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, roc_curve, auc, roc_auc_score
+from sklearn.model_selection import StratifiedKFold, cross_val_score, train_test_split
+
+import warnings
+from sklearn.exceptions import UndefinedMetricWarning
+from sklearn.neural_network import MLPClassifier
+
+__DEBUG__  = True
+
+#
+#
+#
+class Classifier:
+	def __init__(self, CLASS_LABEL, file_types):
+		self.CLASS_LABEL = CLASS_LABEL
+		self.n_classes = 0
+		self.file_types = file_types
+
+	#
+	#
+	#
+	def classify(self, dataset, dataset_filename, testSize=20):
+		y = dataset.Class                            # Labels
+		y = np.array(y)
+		X = dataset.drop(self.CLASS_LABEL, axis=1)   # Features
+		X = np.array(X)
+
+		names = [
+				"Neural Networks"
+				]
+		classifiers = [
+						# Neural Networks
+						# solver: The solver for weight optimization
+						# lbfgs - is an optimizer in the family of quasi-Newton methods
+						# works good with smaller datasets
+						MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=0)
+		]
+
+		# It's not an n-fold cross validation but a split
+		ts = testSize
+		testSize = testSize / 100
+		train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=testSize, random_state=0)
+		n = 0
+		filename_result = dataset_filename + "_" + str(ts) + ".results.txt"
+		file_result = open(filename_result, "w")
+		print("Classifying and saving results in " + filename_result)
+		while n < len(names):
+			clf = classifiers[n]
+			name = names[n]
+			cn = "Classifying with " + name + " . . . . .\n"
+			filename_roc = dataset_filename + "_" + name + "_" + str(ts) + ".png"
+			self.classify_with_split(cn, train_X, test_X, train_y, test_y, clf, file_result, filename_roc)
+			n += 1
+		file_result.close()
+
+	#
+	# It's not an n-fold cross validation but a %age split,
+	# e.g., 80% training and 20% testing
+	# clf is the classifier passed
+	# e.g., NaiveBayes, RandomForest etc
+	#
+	def classify_with_split(self, cn, train_X, test_X, train_y, test_y, clf, file_result, filename_roc):
+		try:
+			result = cn
+			print(result)
+			warnings.filterwarnings("ignore", category=UndefinedMetricWarning)
+
+			class_labels = set(test_y)               # Extract the class labels from the test_y
+			class_labels = list(class_labels)        # Confusion matrix requires class labels in a list
+			cl = list()
+			for names in class_labels:
+				cl.append(str(self.file_types[names]))
+			clf.fit(train_X, train_y)
+			predicted = clf.predict(test_X)
+			expected = test_y
+			class_labels = set(expected)      # Extract the class labels from the true_y
+			class_labels = list(class_labels) # Confusion matrix requires class labels in a list
+			cm = confusion_matrix(expected, predicted, labels=class_labels)
+			# Confusion matrix of 2 classes
+			#   |_A__|_B__
+			# A |_TP_|_FN_
+			# B |_FP_|_TN_
+			#print(cm)
+			report = classification_report(expected, predicted, target_names=cl)
+			print("--- Report ---\n", report)
+			result += "--- Report ---\n" + str(report) + "\n"
+			result += self.classification_results(cm, class_labels)
+
+			file_result.write(result)
+			if __DEBUG__:
+				print(result)
+		except ValueError:
+			print("--- Data ERROR ---")
+			print("Error:Classify::classify_with_split: Value Error!")
+			pass
+
+	#
+	#
+	#
+	def classification_results(self, cm, class_labels):
+		result = ""
+
+		tp = np.diag(cm)
+		fn = cm.sum(axis=1) - np.diag(cm)
+		fp = cm.sum(axis=0) - np.diag(cm)
+		tn = cm.sum() - (tp + fn + fp)
+		classes = len(cm[0])
+		tpr = np.empty(classes, dtype=float)
+		fpr = np.empty(classes, dtype=float)
+		accuracy = np.empty(classes, dtype=float)
+		f1_score = np.empty(classes, dtype=float)
+		precision = np.empty(classes, dtype=float)
+		mean_tpr = mean_fpr = mean_accuracy = mean_f1_score = mean_precision = 0.0
+		for i in range(classes):
+			tpr[i] = 0
+			if (tp[i] + fn[i]) > 0:
+				tpr[i] = tp[i] / (tp[i] + fn[i])   #tp / (tp + fn)
+			mean_tpr = mean_tpr + tpr[i]
+			fpr[i] = 0
+			if (fp[i] + tn[i]) > 0:
+				fpr[i] = fp[i] / (fp[i] + tn[i])   #fp / (fp + tn)
+			mean_fpr = mean_fpr + fpr[i]
+			accuracy[i] = 0
+			if (tp[i] + tn[i] + fp[i] + fn[i]) > 0:
+				accuracy[i] = (tp[i] + tn[i]) / (tp[i] + tn[i] + fp[i] + fn[i])   #(tp + tn) / (tp + tn + fp + fn)
+			mean_accuracy = mean_accuracy + accuracy[i]
+			f1_score[i] = 0
+			if ((2 * tp[i]) + fp[i] + fn[i]) > 0:
+				f1_score[i] = (2 * tp[i]) / ((2 * tp[i]) + fp[i] + fn[i])   #(2 * tp) / ((2 * tp) + fp + fn)
+			mean_f1_score = mean_f1_score + f1_score[i]
+			precision[i] = 0
+			if (tp[i] + fp[i]) > 0:
+				precision[i] = tp[i] / (tp[i] + fp[i])   #tp / tp + fp
+			mean_precision = mean_precision + precision[i]
+			n = class_labels[i]     # Get the specific class label (int) from the confusion matrix class labels
+			c = self.file_types[n]  # Get the type (string label) of the file from the stored file types
+			result += "Reporting results for Class " + c + "\n"
+			result += "   TPR = " + str(tpr[i]) + "\n"
+			result += "   FPR = " + str(fpr[i]) + "\n"
+			result += "   Accuracy = " + str(accuracy[i]) + "\n"
+			result += "   F1 Score = " + str(f1_score[i]) + "\n"
+			result += "   Precision = " + str(precision[i]) + "\n"
+		mean_tpr = mean_tpr / classes
+		mean_fpr = mean_fpr / classes
+		mean_accuracy = mean_accuracy / classes
+		mean_f1_score = mean_f1_score / classes
+		mean_precision = mean_precision / classes
+		result += "Average TPR = " + str(mean_tpr) + "\n"
+		result += "Average FPR = " + str(mean_fpr) + "\n"
+		result += "Average Accuracy = " + str(mean_accuracy) + "\n"
+		result += "Average F1 Score = " + str(mean_f1_score) + "\n"
+		result += "Average Precision = " + str(mean_precision) + "\n\n"
+
+		return result
---
+++ IDS.py
@@ -0,0 +1,101 @@
+# --------------------------------------------------------------------------------------------------------------
+#
+# Filename: IDS.py
+# Author: Shahid Alam (shalam3@gmail.com)
+# Dated: September, 3, 2025
+# IDS => Intrusion Detection System
+#
+# --------------------------------------------------------------------------------------------------------------
+
+from __future__ import print_function
+import os, sys, argparse
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+from Classify import *
+
+__DEBUG__  = True
+CLASS_LABEL = "Class"
+# Classes
+classes = ['Normal', 'Blackhole', 'TCP-SYN', 'PortScan', 'Diversion', 'Overflow']
+
+if __name__=="__main__":
+	print("--- IDS START ---")
+
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument("-csv", type=str)
+	args = parser.parse_args()
+
+	if args.csv == None:
+		print("ERROR:IDS: Missing arguments\nUsage:\n")
+		print("   python IDS.py -csv <csv_filename>")
+		sys.exit(1)
+	csv_filename = args.csv.strip()
+	if os.path.exists(csv_filename) == False:
+		print("File '%s' does not exist"%csv_filename)
+		print("   python IDS.py -csv <csv_filename>")
+		sys.exit(1)
+
+	CL = Classifier(CLASS_LABEL, classes)
+	dataset = pd.read_csv(csv_filename)
+	dataset.rename(columns={"Label": CLASS_LABEL}, inplace=True)
+	print(dataset)
+	if len(dataset) >= 29:   # more than 29 samples
+		if dataset.isna().any(axis=None) == True:
+			dataset.interpolate(method='linear', inplace=True)
+		if dataset.isna().any(axis=None) == True:
+			print("Missing values still present in the dataset")
+			sys.exit(1)
+		CL.classify(dataset, csv_filename)
+	else:
+		print("ERROR:Nothing to classify in file %s"%csv_filename)
+
+	print("--- IDS END ---")
+
+#-------------------------------------------------------------------------------------------------
+# This dataset contains 5000 records of features extracted from Network Port Statistic to protect
+# modern-day computer networks from cyber attacks and are thereby classified into 5 classes
+# 
+# --- 5 CLASSES ---
+# 0 - Normal
+# 1 - Blackhole
+# 2 - TCP-SYN
+# 3 - PortScan
+# 4 - Diversion
+# 5 - Overflow
+# 
+# --- FEATURES ---
+# Switch ID - The switch through which the network flow passed
+# Port Number - The switch port through which the flow passed
+# Received Packets - Number of packets received by the port
+# Received Bytes - Number of bytes received by the port
+# Sent Bytes - Number of bytes sent by the port
+# Sent Packets - Number of packets sent by the port
+# Port alive Duration (S) - The time port has been alive in seconds
+# Packets Rx Dropped - Number of packets dropped by the receiver
+# Packets Tx Dropped - Number of packets dropped by the sender
+# Packets Rx Errors - Number of transmit errors
+# Delta Received Packets - Number of packets received by the port
+# Delta Received Bytes - Number of bytes received by the port
+# Delta Sent Bytes - Number of bytes sent by the port
+# Delta Sent Packets - Number of packets sent by the port
+# Delta Port alive Duration (S) - The time port has been alive in seconds
+# Delta Packets Rx Dropped - Number of packets dropped by the receiver
+# Delta Packets Tx Dropped - Number of packets dropped by the sender
+# Delta Packets Rx Errors - Number of receive errors
+# Delta Packets Tx Errors - Number of transmit errors
+# Connection Point - Network connection point expressed as a pair of the network element identifier and port number.
+# Total Load/Rate - Obtain the current observed total load/rate (in bytes/s) on a link.
+# Total Load/Latest - Obtain the latest total load bytes counter viewed on that link.
+# Load/Rate - Obtain the current observed unknown-sized load/rate (in bytes/s) on a link.
+# Unknown Load/Latest - Obtain the latest unknown-sized load bytes counter viewed on that link.
+# Latest bytes counter - Latest bytes counted in the switch port
+# Checkis_valit - Indicates whether this load was built on valid values.
+# vpn_keyTable ID - Returns the Table ID values.
+# Active Flow Entries - Returns the number of active flow entries in this table.
+# Packets Looked Up - Returns the number of packets looked up in the table.
+# Packets Matched - Returns the number of packets that successfully matched in the table.
+# Max Size - Returns the maximum size of this table.
+# Label - Label types (CLASSES) for intrusions
+#-------------------------------------------------------------------------------------------------
